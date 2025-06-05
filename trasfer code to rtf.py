d4f3de40cd7ee4e96905e6b1011bba0a5a7cc48e
Dollar John <renminjiaoshicm@163.com>
2025-06-05 15:01:52 +0800

    Add files via upload

---
+++ trasfer code to rtf.py
@@ -0,0 +1,16 @@
+from docx import Document
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import RtfFormatter
+c=input("请输入你准备转换为rtf文件的代码：")
+
+doc = Document()
+code = c#'''   '''
+rtf_code = highlight(code, PythonLexer(), RtfFormatter())
+
+# 创建临时RTF文件
+with open("temp.rtf", "w") as f:
+    f.write(rtf_code)
+f.close()
+##doc.add_picture("temp.rtf")  # 作为对象插入
+##doc.save("output.docx")
